ceb48fef47ee5549f5f4789299b229bdec71a479
brikiahmed <briki.ahmed21@gmail.com>
2023-04-04 12:36:29 +0100

    add migration for table users

--- migrations/Version20230315152450.php
+++ migrations/Version20230315152450.php
@@ -20,12 +20,13 @@ final class Version20230315152450 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE accounts (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, note LONGTEXT NOT NULL, added_on DATE NOT NULL, UNIQUE INDEX UNIQ_CAC89EACE7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, note LONGTEXT NOT NULL, added_on DATE NOT NULL, UNIQUE INDEX UNIQ_CAC89EACE7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE users ADD image_file VARCHAR(255) NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE accounts');
+        $this->addSql('DROP TABLE users');
     }
 }
